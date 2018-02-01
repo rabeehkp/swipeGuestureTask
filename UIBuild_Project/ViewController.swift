fd567d916ef7123665a6e15e7b167392aaa0acf8
Rabeeh KP <rabeeh@nfns-macbook-air.local>
2018-02-01 11:14:19 +0530

    Initial Commit

---
+++ UIBuild_Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIBuild_Project
+//
+//  Created by Rabeeh KP on 01/02/18.
+//  Copyright © 2018 Rabeeh KP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
